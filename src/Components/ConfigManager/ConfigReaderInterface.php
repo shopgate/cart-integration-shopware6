8bbd81e50a3c213e3876a744b0b77e5654853ab6
Konstantin Kiritsenko <k@componentk.com>
2021-01-28 21:09:10 +0300

    SGSW6-7 Filtering products by allowed export config type

--- src/Components/ConfigManager/ConfigReaderInterface.php
+++ src/Components/ConfigManager/ConfigReaderInterface.php
@@ -6,6 +6,9 @@ namespace Shopgate\Shopware\Components\ConfigManager;
 
 interface ConfigReaderInterface
 {
+    public const PROD_EXPORT_TYPE_SIMPLE = 'simple';
+    public const PROD_EXPORT_TYPE_VARIANT = 'variant';
+
     public function read(string $salesChannelId = '', bool $fallback = true);
 
     public function get(string $key, $default = '');
--- src/Export/Catalog/Products.php
+++ src/Export/Catalog/Products.php
@@ -2,6 +2,7 @@
 
 namespace Shopgate\Shopware\Export\Catalog;
 
+use Shopgate\Shopware\Components\ConfigManager\ConfigReaderInterface;
 use Shopgate\Shopware\Exceptions\MissingContextException;
 use Shopgate\Shopware\Export\Catalog\Mapping\ProductMapFactory;
 use Shopgate\Shopware\Export\Catalog\Products\ProductSorting;
@@ -12,6 +13,8 @@ use Shopware\Core\Content\Product\SalesChannel\ProductAvailableFilter;
 use Shopware\Core\Content\Product\SalesChannel\ProductListListRoute;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\NotFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\RangeFilter;
 use Throwable;
 
 class Products
@@ -26,6 +29,8 @@ class Products
     private $productSorting;
     /** @var ProductMapFactory */
     private $productMapFactory;
+    /** @var ConfigReaderInterface */
+    private $configReader;
 
     /**
      * @param LoggerInterface $logger
@@ -33,19 +38,22 @@ class Products
      * @param ContextManager $contextManager
      * @param ProductSorting $productSorting
      * @param ProductMapFactory $productMapFactory
+     * @param ConfigReaderInterface $configReader
      */
     public function __construct(
         LoggerInterface $logger,
         ProductListListRoute $productListRoute,
         ContextManager $contextManager,
         ProductSorting $productSorting,
-        ProductMapFactory $productMapFactory
+        ProductMapFactory $productMapFactory,
+        ConfigReaderInterface $configReader
     ) {
         $this->logger = $logger;
         $this->productListRoute = $productListRoute;
         $this->contextManager = $contextManager;
         $this->productSorting = $productSorting;
         $this->productMapFactory = $productMapFactory;
+        $this->configReader = $configReader;
     }
 
     /**
@@ -60,14 +68,11 @@ class Products
         $minSortOrder = max($offset - 1, 0) * $limit;
         $context = $this->contextManager->getSalesContext();
 
-        //todo-konstantin this is a hotfix, please check
-        $filter = new ProductAvailableFilter($context->getSalesChannel()->getId());
-        $filter->addQuery(new EqualsFilter('product.parentId', null));
-
         $criteria = (new Criteria($uids))
             ->setLimit($limit)
             ->setOffset($offset)
-            ->addFilter($filter)
+            ->addFilter(new ProductAvailableFilter($context->getSalesChannel()->getId()))
+            ->addFilter(new EqualsFilter('product.parentId', null))
             ->addAssociations([
                 'media',
                 'properties',
@@ -84,6 +89,19 @@ class Products
                 'children.seoUrls'
             ])
             ->addSorting(...$this->productSorting->getDefaultSorting());
+
+        // Simple products have child_count = 0, Children have 'null', Variants have 0+
+        $types = $this->configReader->get('productTypesToExport');
+        if (count($types) < 2) {
+            $filter = [];
+            if (!in_array(ConfigReaderInterface::PROD_EXPORT_TYPE_SIMPLE, $types, true)) {
+                $filter[] = new EqualsFilter('childCount', 0);
+            }
+            if (!in_array(ConfigReaderInterface::PROD_EXPORT_TYPE_VARIANT, $types, true)) {
+                $filter[] = new RangeFilter('childCount', [RangeFilter::GT => 0]);
+            }
+            $criteria->addFilter(new NotFilter(NotFilter::CONNECTION_OR, $filter));
+        }
         $response = $this->productListRoute->load($criteria, $context);
         $list = [];
         $i = 0;
