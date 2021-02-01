91b8ba80b966dad87326822fec7933dd052a559d
Rainer Skistims <rainer.skistims@shopgate.com>
2021-02-01 21:26:05 +0100

    commit part of item handling in check_cart

--- src/Order/OrderComposer.php
+++ src/Order/OrderComposer.php
@@ -6,8 +6,15 @@ use Shopgate\Shopware\Exceptions\MissingContextException;
 use Shopgate\Shopware\Customer\Mapping\LocationMapping;
 use Shopgate\Shopware\Storefront\ContextManager;
 use ShopgateCart;
+use ShopgateCartBase;
 use ShopgateCartCustomer;
 use ShopgateCartCustomerGroup;
+use Shopware\Core\Checkout\Cart\Cart;
+use Shopware\Core\Checkout\Cart\CartCalculator;
+use Shopware\Core\Checkout\Cart\CartRuleLoader;
+use Shopware\Core\Checkout\Cart\LineItem\LineItem;
+use Shopware\Core\Checkout\Cart\LineItem\LineItemCollection;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Throwable;
 
 class OrderComposer
@@ -16,6 +23,10 @@ class OrderComposer
     private $locationMapping;
     /** @var ContextManager */
     private $contextManager;
+    /** @var CartRuleLoader */
+    private $cartRuleLoader;
+    /** @var CartCalculator */
+    private $cartCalculator;
 
     /**
      * @param LocationMapping $locationMapping
@@ -23,10 +34,14 @@ class OrderComposer
      */
     public function __construct(
         LocationMapping $locationMapping,
-        ContextManager $contextManager
+        ContextManager $contextManager,
+        CartRuleLoader $cartRuleLoader,
+        CartCalculator $cartCalculator
     ) {
         $this->locationMapping = $locationMapping;
         $this->contextManager = $contextManager;
+        $this->cartRuleLoader = $cartRuleLoader;
+        $this->cartCalculator = $cartCalculator;
     }
 
     /**
@@ -43,16 +58,40 @@ class OrderComposer
         }
         $context = $this->contextManager->getSalesContext();
 
-        // todo-rainer implement
+        $shopwareCart = $this->buildShopwareCart($context, $cart);
 
         return [
             "currency" => $context->getCurrency()->getIsoCode(),
-            "external_coupons" => [],
-            "shipping_methods" => [],
-            "payment_methods" => [],
-            "items" => [],
+            "external_coupons" => [], // todo-rainer implement
+            "shipping_methods" => [], // todo-rainer implement
+            "payment_methods" => [], // out of scope
+            "items" => [], // todo-rainer implement
             "customer" => $this->getCartCustomer(),
         ];
+    }
+
+    /**
+     * @param SalesChannelContext $context
+     * @param ShopgateCartBase $cart
+     * @return Cart
+     */
+    protected function buildShopwareCart(SalesChannelContext $context, ShopgateCartBase $cart): Cart
+    {
+        $shopwareCart = $this->cartRuleLoader->loadByToken($context, $context->getToken())->getCart();
+
+        $lineItems = [];
+        foreach ($cart->getItems() as $item) {
+            $productId = $item->getItemNumber();
+            //todo-rainer manage configurable items
+            $lineItems[] = new LineItem($productId, 'product', $productId, $item->getQuantity());
+        }
+
+        $shopwareCart->setLineItems(new LineItemCollection($lineItems));
+
+        $shopwareCart = $this->cartCalculator->calculate($shopwareCart, $context);
+
+        return $shopwareCart;
+
     }
 
     /**
