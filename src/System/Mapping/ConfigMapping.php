5a08097ee834c5dc17f56f10e38fca11b9d440ac
Konstantin Kiritsenko <k@componentk.com>
2022-01-03 22:05:42 +0200

    SG-86 Changed generated file location

--- src/System/Mapping/ConfigMapping.php
+++ src/System/Mapping/ConfigMapping.php
@@ -6,20 +6,33 @@ namespace Shopgate\Shopware\System\Mapping;
 
 use Shopgate\Shopware\System\Configuration\ConfigBridge;
 use ShopgateConfig;
+use Symfony\Component\Filesystem\Filesystem;
 
 class ConfigMapping extends ShopgateConfig
 {
     protected ConfigBridge $configReader;
 
-    /**
-     * @required
-     */
-    public function initShopwareConfig(ConfigBridge $configReader): void
+    public function setConfigBridge(ConfigBridge $configReader): ConfigMapping
     {
         $this->configReader = $configReader;
-        $this->setShopIsActive($this->configReader->get('isActive'));
-        $this->setCustomerNumber($this->configReader->get('customerNumber'));
-        $this->setShopNumber($this->configReader->get('shopNumber'));
-        $this->setApikey($this->configReader->get('apiKey'));
+
+        return $this;
+    }
+
+    public function initShopwareConfig(array $data = []): void
+    {
+        $this->loadArray($data);
+        $this->setLogFolderPath(implode('/', [$this->getLogFolderPath(), $this->getShopNumber()]));
+        $this->setCacheFolderPath(implode('/', [$this->getCacheFolderPath(), $this->getShopNumber()]));
+        $this->setExportFolderPath(implode('/', [$this->getExportFolderPath(), $this->getShopNumber()]));
+    }
+
+    public function initFolderStructure(Filesystem $filesystem): void
+    {
+        array_map(static function (string $path) use ($filesystem) {
+            if (!$filesystem->exists($path)) {
+                $filesystem->mkdir($path);
+            }
+        }, [$this->getLogFolderPath(), $this->getCacheFolderPath(), $this->getExportFolderPath()]);
     }
 }
