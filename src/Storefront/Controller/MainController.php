9b280a5b2dd1b6e048c922fcb168291f0666bc1e
Konstantin Kiritsenko <k@componentk.com>
2024-04-02 19:20:44 -0700

    SW6M-96 Route type change

--- src/Storefront/Controller/MainController.php
+++ src/Storefront/Controller/MainController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(defaults: ['_routeScope' => ['storefront']])]
 class MainController extends StorefrontController
